b588f97c11b240012f967fa24bb06d66e6d867f8
John Slota <john.slota@gmail.com>
2016-04-18 11:34:42 -0600

    links are being displayed and are good to go

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,22 @@
 class LinksController < ApplicationController
   def index
     @user = current_user
+    @links = Link.all
+  end
+
+  def create
+    link = Link.new(link_params)
+    if link.save
+      flash[:notice] = "Successfully saved link."
+    else
+      flash[:notice] = "URL is not valid."
+    end
+    redirect_to links_path
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url, :title)
   end
 end
--- test/integration/user_visits_link_page_test.rb
+++ test/integration/user_visits_link_page_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'pry'
 
 class UserVisitsLinkPageTest < ActiveSupport::TestCase
-  test "user visits link page" do
+  test "user visits link page, uses incorrect url" do
     visit("/")
     click_link("Sign Up")
 
@@ -16,5 +16,38 @@ class UserVisitsLinkPageTest < ActiveSupport::TestCase
     assert page.has_content?("Welcome John.Slota@gmail.com")
     assert page.has_content?("Url")
     assert page.has_content?("Title")
+
+    fill_in('Url', :with => "base")
+    fill_in('Title', :with => "title")
+
+    click_on("Save Link")
+
+    assert page.has_content?("URL is not valid.")
+  end
+
+  test "user visits link page, correct url" do
+    visit("/")
+    click_link("Sign Up")
+
+    fill_in('Email', :with => "John.Slota@gmail.com")
+    fill_in('Password', :with => "123")
+    fill_in('Password confirmation', :with => "123")
+
+    click_on("Save User")
+
+    assert_current_path("/links")
+    assert page.has_content?("Welcome John.Slota@gmail.com")
+    assert page.has_content?("Url")
+    assert page.has_content?("Title")
+
+    fill_in('Url', :with => "http://today.turing.io/outlines/2016-04-18/")
+    fill_in('Title', :with => "title")
+
+    click_on("Save Link")
+
+    save_and_open_page
+    assert page.has_content?("Successfully saved link.")
+    assert page.has_content?("title")
+    assert page.has_content?("http://today.turing.io/outlines/2016-04-18/")
   end
 end
