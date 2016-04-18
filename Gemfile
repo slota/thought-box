a92095f94f429dc47f8eeff786721c626f2a8506
John Slota <john.slota@gmail.com>
2016-04-18 10:28:07 -0600

    user can now sign up

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'capybara'
+gem 'launchy'
+
 
 
 # Use ActiveModel has_secure_password
@@ -35,6 +37,7 @@ gem 'capybara'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry-rails'
 end
 
 group :development do
--- test/integration/user_visits_root_test.rb
+++ test/integration/user_visits_root_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'pry'
 
 class UserVisitsRootTest < ActiveSupport::TestCase
   test "the truth" do
@@ -9,4 +10,22 @@ class UserVisitsRootTest < ActiveSupport::TestCase
     visit('/')
     assert page.has_content?("Log In or Sign Up")
   end
+
+  test "unauthenticated user clicks sign up" do
+    visit("/")
+    click_link("Sign Up")
+
+    assert page.has_content?("Email address")
+    assert page.has_content?("Password")
+    assert page.has_content?("Password confirmation")
+
+    fill_in('Email address', :with => "John.Slota@gmail.com")
+    fill_in('Password', :with => "123")
+    fill_in('Password confirmation', :with => "123")
+
+    click_on("Save User")
+
+    assert_current_path("/links")
+  end
+
 end
